d8ad3f8984e8765a8224154570c44eaee4478e95
srodriguezrobayo <113036583+srodriguezrobayo@users.noreply.github.com>
2024-03-20 20:43:06 -0500

    Update acciones.service.ts

--- src/app/Tablas/shared/acciones.service.ts
+++ src/app/Tablas/shared/acciones.service.ts
@@ -13,7 +13,7 @@ import { catchError } from 'rxjs';
 })
 export class AccionesService {
 
-  public BASE_URL = "http://localhost:3100"
+  public BASE_URL = "https://despliegue-backend-7h6l.onrender.com"
 
   constructor(private http: HttpClient) { }
 
